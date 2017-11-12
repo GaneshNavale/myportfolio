b715c7b18f2fa2f79cd54f0fb08fd1f25e8048c6
GaneshNavale <navaleganesh2011@gmail.com>
2017-11-12 22:04:26 +0530

    navigation changes and added some more info

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,42 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$(document).ready(function(){
+$(document).ready(function () {	
+  $(document).on("scroll", onScroll);
+  
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+    
+    $('a').each(function () {
+        $(this).parent().removeClass('active');
+    })
+    $(this).parent().addClass('active');
+  
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+    }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+    });
+  });
 });
+
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.nav-link a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr('href'));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav-link').removeClass('active');
+            currLink.parent().addClass('active');
+        }
+        else{
+           currLink.parent().removeClass('active');
+        }
+    });
+}
