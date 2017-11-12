6f637958a1a6cac4143c7b53c1cddfe0f34d56e6
GaneshNavale <navaleganesh2011@gmail.com>
2017-11-13 02:59:49 +0530

    mobile navbar collapse issue

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,57 +1,3 @@
-// Isotope filter
-jQuery(document).ready(function($){
-
-  if ( $('.iso-box-wrapper').length > 0 ) { 
-
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
-
-      $container.imagesLoaded(function () {
-
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
-        });
-
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
-
-      });
-
-      //filter items on button click
-
-      $('.filter-wrapper li a').click(function(){
-
-          var $this = $(this), filterValue = $this.attr('data-filter');
-
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false, 
-        }                
-      });             
-
-      // don't proceed if already selected 
-
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
-
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-        return false;
-      }); 
-
-  }
-
-});
-
-
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
 $(function(){
@@ -67,11 +13,6 @@ $(function(){
   // Hide Mobile Menu After Clicking On a link
   $('.nav-link a').click(function(){
         $('.navbar-collapse').collapse('hide');
-    });
-
-  // Nivo lightbox
-  $('.nav-link a').nivoLightbox({
-        effect: 'fadeScale',
     });
 
 });
