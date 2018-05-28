ec6ca0ac8663d962bc75eeb6669e8af09118994c
Ganesh Navale <navaleganesh2011@gmail.com>
2018-05-29 01:02:18 +0530

    configure redis for production

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
+rails_root = Rails.root || File.dirname(__FILE__) + ‘/../..’
+rails_env = Rails.env || ‘development’
+redis_config = YAML.load_file(rails_root.to_s + ‘/config/redis.yml’)
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  config.redis = { url: “redis://#{redis_config[:host]}:#{redis_config[ort]}/12” }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/12' }
+  config.redis = { url: “redis://#{redis_config[:host]}:#{redis_config[ort]}/12” }
 end
