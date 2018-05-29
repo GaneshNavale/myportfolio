9edcb7e629f9ce7082aa074f09ed9d2e4f5e5248
Ganesh Navale <navaleganesh2011@gmail.com>
2018-05-29 13:17:53 +0530

    directly used invironment variable instead of rails environment check

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,7 @@
-host = (Rails.env == 'production') ? ENV['REDIS_HOST'] : 'localhost'
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{host}:6379/12" }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{host}:6379/12" }
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:6379/12" }
 end
